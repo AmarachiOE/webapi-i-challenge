e2894c0a119e493a82f935bc7f80f3bab7dfdb4e
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-22 11:50:47 -0700

    Get /api/users endpoint created

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ const server = express();
 
 server.use(express.json());
 
+////////////// ENDPOINTS //////////////
+
+// GET ALL USERS
+server.get("/api/users", (req, res) => {
+  db
+    .find()
+    .then(users => {
+      res
+        .status(200)
+        .json(users);
+    })
+    .catch(err => {
+      res.json({ error: err, message: "Oops! Something went wrong." });
+    });
+});
+
 server.listen(5000, () => {
-    console.log("\n*** Hey! Server listening on port 500! ***\n");
+  console.log("\n*** Hey! Server listening on port 500! ***\n");
 });
