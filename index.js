333a50474a9c9651a3ecfac775728c146830858b
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-22 12:14:55 -0700

    Post api/users endpoint created. No conditions implemented yet.

--- index.js
+++ index.js
@@ -9,15 +9,34 @@ server.use(express.json());
 
 // GET ALL USERS
 server.get("/api/users", (req, res) => {
-  db
-    .find()
+  db.find()
     .then(users => {
-      res
-        .status(200)
-        .json(users);
+      res.status(200).json(users);
     })
     .catch(err => {
-      res.json({ error: err, message: "Oops! Something went wrong." });
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+    });
+});
+
+// GET USER BY ID
+
+// POST NEW USER
+server.post("/api/users", (req, res) => {
+  const userInfo = req.body;
+  console.log("Req Body: ", userInfo);
+
+  db.insert(userInfo)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          error: "There was an error while saving the user to the database"
+        });
     });
 });
 
